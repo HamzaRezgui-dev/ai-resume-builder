13fc69e26d72adfa58cafc765fa3a0150f80b2bf
ChazzitUP <hamzarezgui.education@gmail.com>
2025-02-23 17:30:04 +0100

    react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
-import {Button} from "@/components/ui/button.tsx";
+import {Outlet} from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      <Button>Click here</Button>
+      <Outlet />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import SignInPage from "@/auth/sign-in";
+import Home from "@/home";
 
+const router = createBrowserRouter(([
+    {
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    },
+    {
+        path: '/auth/sign-in',
+        element: <SignInPage />
+    }
+]))
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
